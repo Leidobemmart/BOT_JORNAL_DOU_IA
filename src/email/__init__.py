b5541dacb60023a8c8472a74eb529cb1e49c9132
Leidobemmart <monitor.lotes.martinelliaud@gmail.com>
2025-12-04 10:21:21 -0300

    Update __init__.py

--- src/email/__init__.py
+++ src/email/__init__.py
@@ -1,1 +1,16 @@
+"""
+Módulo de email para o robô DOU.
+"""
 
+from .sender import EmailSender, SMTPConfig, EmailRecipients, EmailConfig
+from .templates import EmailTemplates
+from .builder import EmailBuilder
+
+__all__ = [
+    'EmailSender',
+    'SMTPConfig',
+    'EmailRecipients',
+    'EmailConfig',
+    'EmailTemplates',
+    'EmailBuilder'
+]
